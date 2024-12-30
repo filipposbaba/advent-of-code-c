d21371485cf00d95e4a3d69948e1ce54679b87a9
Philippos Baba <philippos.babalitis@gmail.com>
2024-12-30 14:30:21 +0200

    Close to finishing pt1

--- day3.c
+++ day3.c
@@ -5,42 +5,52 @@
 // ----------------------------------------
 int getDigitLength(int num) {
     if (num == 0) return 1;
-    return (floor(log((double) num)) + 1);
+    return (floor(log10((double) num)) + 1);
 }
 
 int mulParser(char *instructions) {
-    int num1, num2, product;
+    int num1, num2, length;
+    int product = 0, i = 0;
+
+    while (instructions[i] != '\0') {
+        if (sscanf(&instructions[i], "mul(%d,%d)%n", &num1, &num2, &length) == 2) {
+            if (length == 6 + getDigitLength(num1) + getDigitLength(num2)) {
+                product += num1 * num2;
+                i += length - 1;
+            }
+        }
+
+        i++;
+    }
 
 
-    return 0;
+    return product;
 }
 // ----------------------------------------
 
 
 // ----------------------------------------
 int main(int n, char *args[n]) {
-    // if (n != 2) {
-    //     printf("Please pass in a single file as an argument!\n");
-    //     return -1;
-    // }
-
-    // FILE *inputFile = fopen((char*) args[1], "r");
-    // if (inputFile == NULL) {
-    //     printf("Error: File invalid, or not found!\n");
-    //     return -1;
-    // } 
-
-    char input[] = "fjdksafhjkladsmul(  123,  456)extra";
-    int a, b;
-    int chars_processed;
-
-    // Parse input and track the number of characters processed
-    for (int i = 0; input[i] != '\0'; i++) {
-        if (sscanf(&input[i], "mul(%d,%d)%n", &a, &b, &chars_processed) == 2) {
-            printf("a: %d, b: %d, chars_procc: %d\n", a, b, chars_processed);
-            i += chars_processed;
-        }
+    if (n != 2) {
+        printf("Please pass in a single file as an argument!\n");
+        return -1;
     }
 
+    FILE *inputFile = fopen((char*) args[1], "r");
+    if (inputFile == NULL) {
+        printf("Error: File invalid, or not found!\n");
+        return -1;
+    }
+
+    int product = 0;
+    char instructionLine[4000];
+
+    fgets(instructionLine, sizeof(instructionLine), inputFile);
+    while (!feof(inputFile)) {
+        product += mulParser(instructionLine);
+        fgets(instructionLine, sizeof(instructionLine), inputFile);
+    }
+
+    printf("The product of all the instructions is %d\n", product);
     return 0;
 }
